b89a090f21f299c8c8446fecc1f9423a6a08717f
WinstonH <bjdhjj@gmail.com>
2017-11-24 00:12:20 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
+
+#prepare sshd config
+mkdir -p /var/run/sshd
+sed -i "s/UsePrivilegeSeparation.*/UsePrivilegeSeparation no/g" /etc/ssh/sshd_config \
+&& sed -i "s/UsePAM.*/UsePAM no/g" /etc/ssh/sshd_config \
+&& sed -i "s/PermitRootLogin.*/PermitRootLogin yes/g" /etc/ssh/sshd_config \
+&& sed -i "s/#AuthorizedKeysFile/AuthorizedKeysFile/g" /etc/ssh/sshd_config \
+&& sed -i "s/#X11Forwarding no/X11Forwarding yes/g" /etc/ssh/sshd_config \
+&& sed -i "s/#PermitUserEnvironment no/PermitUserEnvironment yes/g" /etc/ssh/sshd_config \
+&& echo "ForwardX11Trusted yes" >> /etc/ssh/ssh_config
+
+#prepare xauth
+touch /root/.Xauthority
+
 /usr/bin/supervisord -c /etc/supervisord.conf
 exec "$@"
