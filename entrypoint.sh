e8b036efa3370c00b9ba15a468efc72cb4696694
WinstonH <bjdhjj@gmail.com>
2017-11-24 00:17:43 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 #prepare sshd config
-mkdir -p /var/run/sshd
-sed -i "s/UsePrivilegeSeparation.*/UsePrivilegeSeparation no/g" /etc/ssh/sshd_config \
-&& sed -i "s/UsePAM.*/UsePAM no/g" /etc/ssh/sshd_config \
-&& sed -i "s/PermitRootLogin.*/PermitRootLogin yes/g" /etc/ssh/sshd_config \
-&& sed -i "s/#AuthorizedKeysFile/AuthorizedKeysFile/g" /etc/ssh/sshd_config \
-&& sed -i "s/#X11Forwarding no/X11Forwarding yes/g" /etc/ssh/sshd_config \
-&& sed -i "s/#PermitUserEnvironment no/PermitUserEnvironment yes/g" /etc/ssh/sshd_config \
-&& echo "ForwardX11Trusted yes" >> /etc/ssh/ssh_config
+sudo mkdir -p /var/run/sshd
+sudo sed -i "s/UsePrivilegeSeparation.*/UsePrivilegeSeparation no/g" /etc/ssh/sshd_config \
+&& sudo sed -i "s/UsePAM.*/UsePAM no/g" /etc/ssh/sshd_config \
+&& sudo sed -i "s/PermitRootLogin.*/PermitRootLogin yes/g" /etc/ssh/sshd_config \
+&& sudo sed -i "s/#AuthorizedKeysFile/AuthorizedKeysFile/g" /etc/ssh/sshd_config \
+&& sudo sed -i "s/#X11Forwarding no/X11Forwarding yes/g" /etc/ssh/sshd_config \
+&& sudo sed -i "s/#PermitUserEnvironment no/PermitUserEnvironment yes/g" /etc/ssh/sshd_config \
+&& sudo echo "ForwardX11Trusted yes" >> /etc/ssh/ssh_config
 
 #prepare xauth
-touch /root/.Xauthority
+sudo touch /root/.Xauthority
 
-/usr/bin/supervisord -c /etc/supervisord.conf
+sudo /usr/bin/supervisord -c /etc/supervisord.conf
 exec "$@"
